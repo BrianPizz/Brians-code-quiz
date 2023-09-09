fa2d1872ba5212c436bca9aaaadffe501a6c22b1
BrianPizz <bpizzimentijr@outlook.com>
2023-09-09 12:51:56 -0400

    created document variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// Document variables
+const startScreen = document.querySelector('.start');
+const startBtn = document.querySelector('.start-btn');
+const quizScreen = document.querySelector('.quiz');
+const question = document.querySelector('.question');
+const ansA = document.querySelector('.A');
+const ansB = document.querySelector('.B');
+const ansC = document.querySelector('.C');
+const ansD = document.querySelector('.D');
+const feedback = document.querySelector('.feedback');
+const endScreen = document.querySelector('.end');
+const score = document.querySelector('.score');
+const name = document.querySelector('.name');
+const saveScoreBtn = document.querySelector('.save'); 
+const scoreScreen = document.querySelector('.highscore');
+
+
